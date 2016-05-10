36c151490c8514bd62c1a5c136d6cd177aaa16ba
chenrt <chenrt6@mail2.sysu.edu.cn>
2016-05-10 20:59:17 +0800

    modify the path of file to be absolute path and add some comments

--- BDTieBa.py
+++ BDTieBa.py
@@ -1,19 +1,26 @@
 # -*- coding:utf-8 -*-
 import urllib.request
+import os
 import re
 from send_email import send_email
 
 class BDTieBa:
-	def __init__(self, name, main_page):
+	def __init__(self, name, main_page, email_send_list):
 		self.fileName = '{0}.data'.format(name)
 		self.tmpFileName = '{0}.tmpdata'.format(name)
+		self.email_send_list = email_send_list
 		self.name = name
 		self.host = 'http://tieba.baidu.com'
 		self.main_page = main_page
 		self.headers = {'User-Agent' : "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"}
+		self.prefix = os.getcwd() + '/'
 
 
 	def getPage(self, url, args=None):
+		"""get the html content of the given url with args
+
+		Returns: a string whose content is the html
+		"""
 		if args:
 			data = urllib.parse.urlencode(args).encode('utf-8') 
 			req = urllib.request.Request(url, headers = self.headers, data=data)
@@ -25,12 +32,16 @@ class BDTieBa:
 
 
 	def getMainPage(self):
+		"""get the main page content"""
 		return self.getPage(self.host + self.main_page)
 
 
 	def getTopTie(self):
-		# pattern = '<div class="threadlist_title[^>]*>[^<]*<i class="icon-top"[^>]*></i><i[^>]*></i>\s*<a href="([^"]*)".*</div>'
-		# pattern = '<div class="threadlist_title[^>]*>\s*<i[^>]*></i>\s*<i[^>]*></i>\s*<a href="([^"]*)"\s*title="([^"]*).*</div>'
+		"""get the top ties of this website
+
+		Returns: a list of update chapters' relative path
+		such as ['/p/12345', '/p/233666']
+		"""
 		pattern = '<div class="threadlist_title[^>]*>\s*<i[^>]*></i>\s*<i[^>]*></i>\s*<a href="([^"]*)"[^>]*>\s*(.*?)\s*</a>\s*</div>'
 		pattern = re.compile(pattern, re.S)
 		page = self.getMainPage()
@@ -42,6 +53,11 @@ class BDTieBa:
 
 
 	def getAllChapters(self, fileName):
+		"""get all lines in the file
+
+		Returns: a list of string
+		"""
+		fileName = self.prefix + fileName
 		try:
 			with open(fileName, 'r') as f:
 				data = f.readlines()
@@ -52,12 +68,24 @@ class BDTieBa:
 
 
 	def saveChapters(self, fileName, data):
+		"""append new data to the head of the file"""
+		fileName = self.prefix + fileName
+		old_data = self.getAllChapters(fileName)
 		with open(fileName, 'w') as f:
-			for line in data:
+			for line in data + old_data:
 				f.write(line)
 
 
 	def checkNewChapters(self, tops):
+		"""check if there is any new chapters among tops
+		if yes then save them into the achieve file
+
+		Args:
+			tops: a list of paths of the top ties in the main page
+
+		Returns: a list of new contents, each content is consist of a title and an url
+		such as [(title1, url1), (title2, url2)]
+		"""
 		old_paths = self.getAllChapters(self.fileName)
 		new_paths = []
 		new_contents = []
@@ -68,35 +96,46 @@ class BDTieBa:
 				new_paths.append(path)
 
 		if new_paths:
-			self.saveChapters(self.fileName, new_paths + old_paths)
+			self.saveChapters(self.fileName, new_paths)
 		return new_contents
 
 
 	def checkTopTie(self):
+		"""check if there is any new chapters in main page
+		if yes, save them and send email to nitify the user
+		if errors occur when sending email, then save the contents to the tmpFile
+		"""
 		print('Checking {0}...'.format(self.name))
 		tops = self.getTopTie()
 		new_contents = self.checkNewChapters(tops)
+
+		have_new_chaptes = False
 		if new_contents:
+			have_new_chaptes = True
 			send_result = self.send_noticification(new_contents)
 			for (host, error) in send_result:
 				print('Error happend when send email to {0}, {1}'.format(host, error))
 				self.saveChapters(self.tmpFileName, new_contents)
-		else:
-			send_failed_contents = self.check_send_failed_contents()
-			if not send_failed_contents:
-				print('No update for {0}'.format(self.name))
+
+		send_failed_contents = self.check_send_failed_contents()
+
+		# if no new chapters or again fail to send failed contents
+		if not (have_new_chaptes or send_failed_contents):
+			print('No update for {0}'.format(self.name))
 
 
 	def send_noticification(self, new_contents):
+		"""send email to notify the users about the new contents"""
 		mail_title = '{0}更新了{1}章'.format(self.name, len(new_contents))
 		content = mail_title + "：\n"
 		for (url, title) in new_contents:
 			content += '{0}\n{1}\n\n'.format(title, url)
 		print('sending email...\n{0}'.format(content))
-		return send_email(['1101925754@qq.com'], mail_title, content)
+		return send_email(self.email_send_list, mail_title, content)
 
 
 	def check_send_failed_contents(self):
+		"""send the failed contents again"""
 		try:
 			with open(self.tmpFileName, 'r') as f:
 				send_failed_contents = f.readlines();
@@ -109,4 +148,4 @@ class BDTieBa:
 				if not send_result:
 					with open(self.tmpFileName, 'w') as f:
 						pass
-			return send_failed_contents
+			return send_failed_contents == {}
--- send_email.py
+++ send_email.py
@@ -5,7 +5,8 @@ import email.mime.text
 
 
 EMAIL_HOST = 'zuoyela_jacket@sina.com'  # your email address
-EMAIL_PASS = '*'   # your email account password
+EMAIL_PASS = 'caibudao233666'   # your email account password
+# EMAIL_PASS = '*'   # your email account password
 SMTP_SERVER = 'smtp.sina.com'   # your email smtp server domain name
 SMTP_PORT = '25'                # smtp protocol use port 25 to send email
 
@@ -25,4 +26,4 @@ def send_email(to_list, sub, content):
 	return result
 
 # Example tl use the function:
-#send_email(['1101925754@qq.com', 'insysujacket@gmail.com'], 'Testing title', 'I am a test and I am testing the send_email function')
+# send_email(['1101925754@qq.com', 'insysujacket@gmail.com'], 'Testing title', 'I am a test and I am testing the send_email function')
